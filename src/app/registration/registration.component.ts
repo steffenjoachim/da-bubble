9948081f114bb96b29173fccb237008a8b6942b1
Bünyamin Ilhan <ilhan.buenyain@gmail..com>
2023-08-26 15:43:47 +0200

    add form validation

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,21 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FirebaseService } from '../services/firebase.service';
+
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent {
-constructor(public firebaseService: FirebaseService) {
-}
+  @ViewChild('myForm') myForm!: ElementRef;
+  @ViewChild('emailField') emailField!: ElementRef;
+  @ViewChild('passwordField') passwordField!: ElementRef;
+  @ViewChild('passwordField2') passwordField2!: ElementRef;
 
-  users: any = {
+  constructor(public firebaseService: FirebaseService) { }
+
+  users = {
     email: '',
     password: ''
-  }
+  };
 
   addUser() {
+    const emailInputElement = this.emailField.nativeElement;
+    const passwordInputElement = this.passwordField.nativeElement;
+    const password2InputElement = this.passwordField2.nativeElement;
+
+    emailInputElement.disabled = true;
+    passwordInputElement.disabled = true;
+    password2InputElement.disabled = true;
+
+    this.users.email = emailInputElement.value;
+    this.users.password = passwordInputElement.value;
     this.firebaseService.addUser(this.users);
-    console.log('registered')
   }
 }
