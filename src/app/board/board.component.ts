b718d930aa29df9b51feb575fce1b62b1f0f85c7
steffenjoachim <steffenjoachim.schanze@gmail.com>
2023-08-28 21:12:55 +0200

    board changes

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -8,6 +8,7 @@ import { Auth } from '@angular/fire/auth';
   styleUrls: ['./board.component.scss']
 })
 export class BoardComponent implements OnInit {
+  open: boolean = true;
 
   constructor(
     private firebase: FirebaseService,
@@ -23,6 +24,26 @@ export class BoardComponent implements OnInit {
     this.firebase.setLogoVisible(false);
   }
 
+  closeSideBar(){
+    if (this.open) {
+      document.getElementById('side-bar')?.classList.add('d-none');
+      document.getElementById('close-x')?.classList.add('d-none');
+      document.getElementById('open-arrow')?.classList.remove('d-none');
+      document.getElementById('close-workspace')?.classList.add('d-none');
+      document.getElementById('open-workspace')?.classList.remove('d-none');
+      this.open = false;
+    } else {
+      document.getElementById('side-bar')?.classList.remove('d-none');
+      document.getElementById('close-x')?.classList.remove('d-none');
+      document.getElementById('open-arrow')?.classList.add('d-none');
+      document.getElementById('close-workspace')?.classList.remove('d-none');
+      document.getElementById('open-workspace')?.classList.add('d-none');
+      this.open = true;
+    }
+  }
 
+  closeThread(){
+    document.getElementById('thread')?.classList.add('d-none');
+  }
 
 }
